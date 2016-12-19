4f29dcaf3494318e80b60ea38e0e049adbca1850
merafour@qq.com <TheTravels>
2016-12-19 15:05:38 +0800

    first commit

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'mx_01_uart' 
+ * Target:  'mx_01_uart' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
